67cd738cee04cd079d1049228fc0ee231d137d8f
liverpl1920 <liverpl1920@gmail.com>
2026-06-13 09:54:00 +0900

    feat: 進捗更新時に差分を確認するダイアログを表示する (#357)

--- spec/system/books/progress_update_spec.rb
+++ spec/system/books/progress_update_spec.rb
@@ -38,4 +38,70 @@ RSpec.describe '進捗更新', type: :system do
       expect(page).to have_field('direct_page')
     end
   end
+
+  describe '現在ページ入力フォーム（JS）', js: true do
+    before do
+      login_as(user, scope: :user)
+      visit book_path(book)
+    end
+
+    context 'ダイアログでOKを選択した場合' do
+      it '進捗が正常に更新される' do
+        fill_in 'direct_page', with: 30
+
+        accept_confirm("新しく 30 ページ読み進めましたか？（累計: 30 ページ）") do
+          click_button '更新する'
+        end
+
+        expect(page).to have_current_path(book_path(book))
+        expect(page).to have_text('進捗を更新しました')
+        expect(page).to have_text('30 / 200')
+      end
+    end
+
+    context 'ダイアログでキャンセルを選択した場合' do
+      it '進捗の更新がキャンセルされ、入力値が維持される' do
+        fill_in 'direct_page', with: 40
+
+        dismiss_confirm("新しく 40 ページ読み進めましたか？（累計: 40 ページ）") do
+          click_button '更新する'
+        end
+
+        expect(page).to have_current_path(book_path(book))
+        expect(page).to have_no_text('進捗を更新しました')
+        expect(find_field('direct_page').value).to eq '40'
+        expect(book.reload.current_page).to eq 0
+      end
+    end
+
+    context '差分が0の場合' do
+      it '差分が0のメッセージが表示され、更新できる' do
+        fill_in 'direct_page', with: 0
+
+        accept_confirm("進捗を更新しますか？（累計: 0 ページ）") do
+          click_button '更新する'
+        end
+
+        expect(page).to have_text('進捗を更新しました')
+      end
+    end
+
+    context '差分が負の場合' do
+      before do
+        book.update!(current_page: 50)
+        visit book_path(book)
+      end
+
+      it '差分が負のメッセージが表示され、更新（戻す）できる' do
+        fill_in 'direct_page', with: 30
+
+        accept_confirm("現在のページ数を戻しますか？（累計: 30 ページ）") do
+          click_button '更新する'
+        end
+
+        expect(page).to have_text('進捗を更新しました')
+        expect(page).to have_text('30 / 200')
+      end
+    end
+  end
 end
